1ca45491edb78e54cb59fad2be0328bc00edebd2
Your NameEmmanuel <agbavweisaac@gmail.com>
2023-05-29 01:35:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const mContract = 218570587
 const gAContract = 218571031
 const gASAContract = 218571694
 const mmContract = 218572062
+const TT1 = 209996092,
+	TT2 = 209996105,
+	TT3 = 209996111,
+	TT4 = 209996154,
+	TT5 = 209996166
+const sConAddr = 'ZWIHHJE6L7XJNRQW6UY4LS4QTK2TJOGPEQNELY5AFSRPBMZKICXSAEXBLM'
 
 // ;(async () => {
 // 	const asset = parseInt(process.env.TRUST_ID)
@@ -78,27 +84,94 @@ const mmContract = 218572062
 // })().catch((e) => {
 // 	console.log(e)
 // })
+// ;(async () => {
+// 	console.log(`[+] Retrieving suggested transaction params`)
+// 	const suggestedParams = await algodClient.getTransactionParams().do()
+
+// 	console.log(`[+] Creating User Opt-in transaction to the Staking contract`)
+// 	const user_OptInTxn = algosdk.makeApplicationOptInTxnFromObject({
+// 		appIndex: sContract,
+// 		from: user.addr,
+// 		note: enc.encode('Staking contract Opt-in call'),
+// 		suggestedParams,
+// 	})
+// 	console.log(`[+] Signing transaction`)
+// 	const signedTxn = user_OptInTxn.signTxn(user.sk)
+// 	console.log(`[+] Publishing signed transactions`)
+// 	await algodClient.sendRawTransaction(signedTxn).do()
+// 	await algosdk.waitForConfirmation(
+// 		algodClient,
+// 		user_OptInTxn.txID().toString(),
+// 		3
+// 	)
+// 	console.log(`[+] The transaction should be signed by now`)
+// })().catch((e) => {
+// 	console.log(e)
+// })
+// ;(async () => {
+// 	const accountInfo = await algodClient.accountInformation(user.addr).do()
+// 	console.log(accountInfo)
+// })().catch((e) => {
+// 	console.log(e)
+// })
 ;(async () => {
 	console.log(`[+] Retrieving suggested transaction params`)
 	const suggestedParams = await algodClient.getTransactionParams().do()
 
-	console.log(`[+] Creating User Opt-in transaction to the Staking contract`)
-	const user_OptInTxn = algosdk.makeApplicationOptInTxnFromObject({
+	console.log(`[+] Creating User asset transfer transaction`)
+	const user_aXferTxn =
+		algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+			amount: 1,
+			assetIndex: TT1,
+			closeRemainderTo: undefined,
+			from: user.addr,
+			note: enc.encode('Transfer of 0.000001 TT1FT'),
+			rekeyTo: undefined,
+			revocationTarget: undefined,
+			to: sConAddr,
+			suggestedParams,
+		})
+
+	console.log(`[+] Creating User Stake transaction to the Staking contract`)
+	const user_NoOptTxn = algosdk.makeApplicationNoOpTxnFromObject({
 		appIndex: sContract,
+		accounts: [user.addr],
+		appArgs: [enc.encode('User_stake'), algosdk.encodeUint64(1)],
+		boxes: [{ appIndex: sContract, name: enc.encode([0, user.addr]) }],
 		from: user.addr,
-		note: enc.encode('Staking contract Opt-in call'),
+		foreignAssets: [parseInt(process.env.TRUST_ID), TT1, TT2, TT3, TT4, TT5],
+		note: enc.encode('Staking contract stake call'),
 		suggestedParams,
 	})
-	console.log(`[+] Signing transaction`)
-	const signedTxn = user_OptInTxn.signTxn(user.sk)
+
+	console.log(`[+] Grouping the transactions`)
+	const txnArray = [user_aXferTxn, user_NoOptTxn]
+	const txnGroup = algosdk.assignGroupID(txnArray)
+
+	console.log(`[+] Signing transactions`)
+	const userSignedTxn1 = txnGroup[0].signTxn(user.sk)
+	const userSignedTxn2 = txnGroup[1].signTxn(user.sk)
+
+	const signedTxns = [userSignedTxn1, userSignedTxn2]
+
 	console.log(`[+] Publishing signed transactions`)
-	await algodClient.sendRawTransaction(signedTxn).do()
+	await algodClient.sendRawTransaction(signedTxns).do()
 	await algosdk.waitForConfirmation(
 		algodClient,
-		user_OptInTxn.txID().toString(),
+		user_aXferTxn.txID().toString(),
 		3
 	)
 	console.log(`[+] The transaction should be signed by now`)
 })().catch((e) => {
 	console.log(e)
 })
+// ;(async () => {
+// 	const accountInfo = await algodClient.accountInformation(user.addr).do()
+// 	const appInfo = await algodClient.getApplicationByID(sContract).do()
+// 	// const appAddr = await algosdk.logic.getApplicationAddress(
+// 	// 	sContract
+// 	// )
+// 	// console.log(appInfo,appAddr)
+// })().catch((e) => {
+// 	console.log(e)
+// })
